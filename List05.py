c4a7807171c9170acd6812c96537454f3f678b2a
Nurbek <nurbek.saitqulov@gmail.com>
2022-12-02 16:06:33 +0500

    List05.py

--- List05.py
+++ List05.py
@@ -9,4 +9,7 @@ def main(numbers1, numbers2):
     Returns:
         list: return answer
     """
-    return
+    numbers2.insert(0, numbers1.pop(-1))
+    numbers1.extend(numbers2)
+    return numbers1
+print(main([6, 8, 1], [3, 5, 7]))
